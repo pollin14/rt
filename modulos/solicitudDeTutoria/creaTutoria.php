d19873295fe4d42901fd0552a17f0c468760e9dc
Victor <victor aguilar@victor-PC>
2012-09-21 12:50:30 -0500

    Ya se envia el mensaje "En etapa uno" en la ventana tutorias

--- modulos/solicitudDeTutoria/creaTutoria.php
+++ modulos/solicitudDeTutoria/creaTutoria.php
@@ -3,6 +3,7 @@ session_start();
 include "../../configuracion.php";
 include "../../lib/php/utils.php";
 include "../../lib/php/queries.php";
+include "../../modulos/tutorias/lib/php/funciones.php";
 
 header('Content-Type: text/html; charset=UTF-8');
 header('refresh:3; url="../../modulos/mensajesPrivados/bandejaDeEntrada.php"'); 
@@ -33,24 +34,18 @@ $query = sprintf('insert into MensajesPrivados
 
 if(! $db -> query($query)) echo ("Error al enviar mensaje de confirmacion");
 
-$query = sprintf("insert into Tutorias (estudiante,idTema) values(%d,%d);",
+$insert = sprintf("insert into Tutorias (estudiante,idTema) values(%d,%d);",
         $_GET['idTutorado'],$_GET['idTema']);
+
+if(! $db->query($insert) ){
+    $html = "<p>Error. La tutoria ya fue creada.</p>"; 
+	print($html);
+	exit();
+}
 
 $idTutoria = $db->insert_id;
 
-//Insertamos un mensaje en la tutoria recien creada para sea detectada en las
-//estadisticas.
-
-//$mensaje = "El primer mensaje de la tutoria";
-//$query = sprintf('call guardaMensaje(%d,%d,"%s","%s",false,%d,@mili)',
-//		$idTutoria,$_SESSION['idUsuario'], getActualData(),$mensaje, 1);
-//
-//if (! $db->query($query)) die("Error." . $db->error);
-
-if(! $db->query($query) ){
-    $html = "<p>Error al crear la tutoria.</p><p> Tutor"; 
-    $html .=$_GET['idTutorado'] . " -> Tema" .$_GET['idTema'] . "</p>";
-}
+saveMensaje($idTutoria,$de,1,1,"Etapa uno",$db);
 
 $html = '<center><p style="margin-top:auto;margin-bottom">Tutoria creada. Direccionando en 3 seg...</p></center>'; 
 
