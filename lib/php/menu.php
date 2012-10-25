0b9a9c47ec145d9584b5c4f01f1887ec0d028b5b
Victor <victor aguilar@victor-PC>
2012-10-24 19:24:23 -0500

    Se agrego un boton para ir a la pantalla misTutorias

--- lib/php/menu.php
+++ lib/php/menu.php
@@ -1,8 +1,11 @@
 <?php
 
-echo '<div style="flaot:right;">';
-echo '<button name="home" onclick=window.location="../../modulos/loged/loged.php">
+echo '<div style="flaot:right; margin-left:30px;">';
+echo '<button name="home" onclick=window.location="../loged/loged.php">
 		<img src="../../lib/img/escritorio.png"/>HOME
+	</button>';
+echo '<button name="misTutorias" onclick=window.location="../misTutorias/misTutorias.php">
+		<img src="../../lib/img/chat.png"/>Mis Tutorías
 	</button>';
 echo '</div>';
 echo '<div sytle="clear:both"></div>';
