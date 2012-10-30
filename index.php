0bd44f120df166505d9abc82c49378d159b53df8
Victor <victor aguilar@victor-PC>
2012-10-30 16:45:36 -0600

    Se eilimino la descripcion y el hint del envio de archivo al chat. Y se puedo el texto a la izquierda en los mensajes del chat.

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
     </table>
 </form>
 <p></p>
-    <p><strong><a href="Privacidad.php">Política  de privacidad y manejo de datos personales</a><a href="modulos/estadisticas/index.php">.</a></strong></p>
+    <p><strong><a href="Privacidad.php">Política  de privacidad y manejo de datos personales</a><a href="modulos/estadisticas/estadisticas.php">.</a></strong></p>
   <tr><td valign="top" height="20">
 <?php include "lib/php/pieDePagina.php" ?>
 </td></tr></table>
